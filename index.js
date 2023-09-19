a4f66206d82cd9df3148bcf6995914e02c694e78
Eric Bernardi <eric.bernardi@pulsati.com.br>
2023-09-19 17:38:29 -0300

    FIX: Adjustments to improve performance

--- index.js
+++ index.js
@@ -4,29 +4,67 @@ import bcrypt from "bcrypt";
 import DB from "./db.js";
 import User from "./Models/User.js";
 import Course from "./Models/Course.js";
-import dotenv from 'dotenv';
-
+import dotenv from "dotenv";
 
 dotenv.config();
 const server = express();
 const port = 3000;
-let users = new Array<User>[];
-let courses = new Array<Course>[];
+let users = new Array() < User > [];
+let courses = new Array() < Course > [];
 
 const database = new DB();
 
-database.getUsers().then((response) => {
-  users = response;
-});
-database.getCourses().then((response) => {
-  courses = response;
-  console.log(courses);
-});
+function insertUser() {
+  const password = "123";
+  const saltRounds = 10;
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    if (err) {
+      console.error("Erro ao criar o hash da senha:", err);
+    } else {
+      database
+        .setUsers({ username: "fire", login_user: "fire", password: hash })
+        .then((res, err) => {
+          console.log(res);
+        });
+    }
+  });
+}
+
+// insertUser();
+
+async function getUsers() {
+  return database.getUsers().then((response, err) => {
+    if (err) return `Error: ${err}`;
+    return response;
+  });
+}
+
+async function insertCourses(course) {
+  try {
+    const result = await database.setCourses(course);
+    if (result) return { ok: true };
+  } catch (error) {
+    console.error({ "Erro no curso": error });
+    throw new Error(error);
+  }
+}
+
+async function getCourses() {
+  return database.getCourses().then((response, err) => {
+    if (err) return `Error: ${err}`;
+    console.log(response);
+    return response;
+  });
+}
+
+getCourses();
 
 server.use(express.json());
 
 server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  if (req.headers.origin === "http://localhost:3001") {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  }
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
@@ -35,15 +73,16 @@ server.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
   next();
 });
 
-server.post("/login", (req, res) => {
+server.post("/login", async (req, res) => {
   const username = req.body.credentials.username;
   const password = req.body.credentials.password;
   const saltRounds = 10;
-  let hashPassword = "";
+  // var hashPassword = "";
+
+  // Método usado para criptografar a senha
 
   // Método usado para criptografar a senha
   // bcrypt.hash(password, saltRounds, function(err, hash) {
@@ -55,12 +94,18 @@ server.post("/login", (req, res) => {
   // })
 
   try {
-    bcrypt.compare(password, hashPassword, function (err, result) {
+    let users = await getUsers();
+
+    const user = users.filter((user) => {
+      return user.login_user == username;
+    });
+    console.log(user[0]);
+    bcrypt.compare(password, user[0].password, function (err, result) {
       if (err) {
         console.log("Erro ao comparar as senhas: ", err);
         return res.status(500).send("Ocorreu um erro", err);
       } else {
-        if (username === "Teste" && result) {
+        if (username === user[0].login_user && result) {
           const token = jsonwebtoken.sign(
             { user: JSON.stringify(username) },
             process.env.KEY_TO_ENCRYPT_PASSWORDS,
@@ -86,22 +131,22 @@ server.get("/", (req, res) => {
   }
 });
 
-server.get("/cursos/:index", (req, res) => {
+server.get("/cursos/:index", async (req, res) => {
   const { index } = req.params;
+  const cursos = await getCourses();
 
   return res.json(cursos[index == 0 ? index : index - 1]);
 });
 
-server.get("/cursos", (req, res) => {
+server.get("/cursos", async (req, res) => {
+  const cursos = await getCourses();
   return res.json(cursos);
 });
 
-server.post("/cursos", (req, res) => {
-  const name = req.body.data;
-  const idCurso = cursos.length + 1;
-  cursos.push({ id: idCurso, curso: name });
-
-  return res.json(cursos);
+server.post("/cursos", async (req, res) => {
+  const response = await insertCourses(req.body.data);
+  console.log(response);
+  return res.json(response);
 });
 
 server.put("/cursos/:index", (req, res) => {
